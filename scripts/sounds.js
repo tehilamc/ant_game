28473057e81f89077de410c95bac7fe42165de68
Avigail Siman Tov <avigailsi123@gmail.com>
2022-08-22 14:52:36 +0300

    שמע בכל הדפים

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -6,12 +6,12 @@ button.addEventListener("click", () => {
   if (audio.paused) {
     audio.volume = 0.2;
     audio.play();
-    icon.classList.remove("fa-volume-up");
-    icon.classList.add("fa-volume-mute");
-  } else {
-    audio.pause();
     icon.classList.remove("fa-volume-mute");
     icon.classList.add("fa-volume-up");
+  } else {
+    audio.pause();
+    icon.classList.remove("fa-volume-up");
+    icon.classList.add("fa-volume-mute");
   }
   button.classList.add("fade");
 });
