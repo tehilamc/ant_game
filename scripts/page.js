81847d8ec0d505024aa1778a5dc545a4d5c0a905
Avigail Siman Tov <avigailsi123@gmail.com>
2022-08-27 22:09:38 +0300

    page.html

--- scripts/page.js
+++ scripts/page.js
@@ -50,57 +50,57 @@ const brands = [
   {
     iconName: "blackberry",
     brandName: "BlackBerry",
-    color: "#000000"
+    color: "#000000" 
   },
   {
     iconName: "blogger",
     brandName: "Blogger",
-    color: "#f57d00"
+    color: "#f57d00" 
   },
   {
     iconName: "bluetooth",
     brandName: "Bluetooth",
-    color: "#3b5998"
+    color: "#3b5998" 
   },
   {
     iconName: "bootstrap",
     brandName: "Bootstrap",
-    color: "#553c7b"
+    color: "#553c7b" 
   },
   {
     iconName: "chrome",
     brandName: "Google Chrome",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "codepen",
     brandName: "CodePen",
-    color: "#111111"
+    color: "#111111" 
   },
   {
     iconName: "cpanel",
     brandName: "cPanel",
-    color: "#ff6c2c"
+    color: "#ff6c2c" 
   },
   {
     iconName: "css3-alt",
     brandName: "CSS3",
-    color: "#264de4"
+    color: "#264de4" 
   },
   {
     iconName: "dev",
     brandName: "Dev",
-    color: "#111111"
+    color: "#111111" 
   },
   {
     iconName: "dhl",
     brandName: "DHL",
-    color: "#ba0c2f"
+    color: "#ba0c2f" 
   },
   {
     iconName: "digital-ocean",
     brandName: "DigitalOcean",
-    color: "#008bcf"
+    color: "#008bcf" 
   },
   {
     iconName: "discord",
@@ -110,57 +110,57 @@ const brands = [
   {
     iconName: "docker",
     brandName: "Docker",
-    color: "#0db7ed"
+    color: "#0db7ed" 
   },
   {
     iconName: "dribbble",
     brandName: "Dribbble",
-    color: "#ea4c89"
+    color: "#ea4c89" 
   },
   {
     iconName: "dropbox",
     brandName: "Dropbox",
-    color: "#007ee5"
+    color: "#007ee5" 
   },
   {
     iconName: "drupal",
     brandName: "Drupal",
-    color: "#0077c0"
+    color: "#0077c0" 
   },
   {
     iconName: "ebay",
     brandName: "eBay",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "edge",
     brandName: "Microsoft Edge",
-    color: "#0078d7"
+    color: "#0078d7" 
   },
   {
     iconName: "ember",
     brandName: "Ember.js",
-    color: "#f23819"
+    color: "#f23819" 
   },
   {
     iconName: "ethereum",
     brandName: "Ethereum",
-    color: "#666666"
+    color: "#666666" 
   },
   {
     iconName: "etsy",
     brandName: "Etsy",
-    color: "#d5641c"
+    color: "#d5641c" 
   },
   {
     iconName: "evernote",
     brandName: "Evernote",
-    color: "#2dbe60"
+    color: "#2dbe60" 
   },
   {
     iconName: "facebook-square",
     brandName: "Facebook",
-    color: "#3b5998"
+    color: "#3b5998" 
   },
   {
     iconName: "fedex",
@@ -170,52 +170,52 @@ const brands = [
   {
     iconName: "firefox",
     brandName: "Firefox",
-    color: "#e66000"
+    color: "#e66000" 
   },
   {
     iconName: "font-awesome",
     brandName: "Font Awesome",
-    color: "#228ae6"
+    color: "#228ae6" 
   },
   {
     iconName: "free-code-camp",
     brandName: "freeCodeCamp",
-    color: "#006400"
+    color: "#006400" 
   },
   {
     iconName: "git-alt",
     brandName: "Git",
-    color: "#f1502f"
+    color: "#f1502f" 
   },
   {
     iconName: "github",
     brandName: "GitHub",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "google",
     brandName: "Google",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "google-play",
     brandName: "Google Play",
-    color: "#3bccff"
+    color: "#3bccff" 
   },
   {
     iconName: "grunt",
     brandName: "Grunt",
-    color: "#fba919"
+    color: "#fba919" 
   },
   {
     iconName: "gulp",
     brandName: "gulp.js",
-    color: "#db4446"
+    color: "#db4446" 
   },
   {
     iconName: "html5",
     brandName: "HTML5",
-    color: "#e34f26"
+    color: "#e34f26" 
   },
   {
     iconName: "imdb",
@@ -225,82 +225,82 @@ const brands = [
   {
     iconName: "instagram",
     brandName: "Instagram",
-    color: "#e1306c"
+    color: "#e1306c" 
   },
   {
     iconName: "internet-explorer",
     brandName: "Internet Explorer",
-    color: "#1ebbee"
+    color: "#1ebbee" 
   },
   {
     iconName: "itunes",
     brandName: "iTunes",
-    color: "#ea4cc0"
+    color: "#ea4cc0" 
   },
   {
     iconName: "java",
     brandName: "Java",
-    color: "#5382a1"
+    color: "#5382a1" 
   },
   {
     iconName: "js-square",
     brandName: "JavaScript",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "jsfiddle",
     brandName: "JSFiddle",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "kickstarter",
     brandName: "Kickstarter",
-    color: "#2bde73"
+    color: "#2bde73" 
   },
   {
     iconName: "laravel",
     brandName: "Laravel",
-    color: "#f55247"
+    color: "#f55247" 
   },
   {
     iconName: "less",
     brandName: "Less",
-    color: "#00316e"
+    color: "#00316e" 
   },
   {
     iconName: "linkedin",
     brandName: "LinkedIn",
-    color: "#0077b5"
+    color: "#0077b5" 
   },
   {
     iconName: "linux",
     brandName: "Linux",
-    color: "#000000"
+    color: "#000000" 
   },
   {
     iconName: "mailchimp",
     brandName: "Mailchimp",
-    color: "#239ab9"
+    color: "#239ab9" 
   },
   {
     iconName: "medium",
     brandName: "Medium",
-    color: "#00ab6c"
+    color: "#00ab6c" 
   },
   {
     iconName: "meetup",
     brandName: "Meetup",
-    color: "#e0393e"
+    color: "#e0393e" 
   },
   {
     iconName: "microsoft",
     brandName: "Microsoft",
-    color: "#111111"
+    color: "#111111" 
   },
   {
     iconName: "napster",
     brandName: "Napster",
-    color: "#111111"
+    color: "#111111" 
   },
   {
     iconName: "node",
@@ -310,147 +310,147 @@ const brands = [
   {
     iconName: "npm",
     brandName: "npm",
-    color: "#cc3534"
+    color: "#cc3534" 
   },
   {
     iconName: "opera",
     brandName: "Opera",
-    color: "#cc0f16"
+    color: "#cc0f16" 
   },
   {
     iconName: "patreon",
     brandName: "Patreon",
-    color: "#f96854"
+    color: "#f96854" 
   },
   {
     iconName: "paypal",
     brandName: "PayPal",
-    color: "#003087"
+    color: "#003087" 
   },
   {
     iconName: "php",
     brandName: "PHP",
-    color: "#8892be"
+    color: "#8892be" 
   },
   {
     iconName: "pinterest",
     brandName: "Pinterest",
-    color: "#bd081c"
+    color: "#bd081c" 
   },
   {
     iconName: "playstation",
     brandName: "PlayStation",
-    color: "#003087"
+    color: "#003087" 
   },
   {
     iconName: "product-hunt",
     brandName: "Product Hunt",
-    color: "#da552f"
+    color: "#da552f" 
   },
   {
     iconName: "python",
     brandName: "Python",
-    color: "#4584b6"
+    color: "#4584b6" 
   },
   {
     iconName: "quora",
     brandName: "Quora",
-    color: "#a82400"
+    color: "#a82400" 
   },
   {
     iconName: "react",
     brandName: "React",
-    color: "#00d8ff"
+    color: "#00d8ff" 
   },
   {
     iconName: "reddit",
     brandName: "reddit",
-    color: "#ff4500"
+    color: "#ff4500" 
   },
   {
     iconName: "safari",
     brandName: "Safari",
-    color: "#0fb5ee"
+    color: "#0fb5ee" 
   },
   {
     iconName: "sass",
     brandName: "Sass",
-    color: "#cd6799"
+    color: "#cd6799" 
   },
   {
     iconName: "skype",
     brandName: "Skype",
-    color: "#00aff0"
+    color: "#00aff0" 
   },
   {
     iconName: "slack",
     brandName: "Slack",
-    color: "#3eb991"
+    color: "#3eb991" 
   },
   {
     iconName: "snapchat-square",
     brandName: "Snapchat",
-    color: "#fffc00"
+    color: "#fffc00" 
   },
   {
     iconName: "soundcloud",
     brandName: "SoundCloud",
-    color: "#ff8800"
+    color: "#ff8800" 
   },
   {
     iconName: "spotify",
     brandName: "Spotify",
-    color: "#1db954"
+    color: "#1db954" 
   },
   {
     iconName: "squarespace",
     brandName: "Squarespace",
-    color: "#222222"
+    color: "#222222" 
   },
   {
     iconName: "stack-overflow",
     brandName: "Stack Overflow",
-    color: "#f48024"
+    color: "#f48024" 
   },
   {
     iconName: "stripe",
     brandName: "Stripe",
-    color: "#00afe1"
+    color: "#00afe1" 
   },
   {
     iconName: "trello",
     brandName: "Trello",
-    color: "#0079bf"
+    color: "#0079bf" 
   },
   {
     iconName: "tripadvisor",
     brandName: "TripAdvisor",
-    color: "#00af87"
+    color: "#00af87" 
   },
   {
     iconName: "twitch",
     brandName: "Twitch",
-    color: "#6441a5"
+    color: "#6441a5" 
   },
   {
     iconName: "twitter",
     brandName: "Twitter",
-    color: "#1da1f2"
+    color: "#1da1f2" 
   },
   {
     iconName: "uber",
     brandName: "Uber",
-    color: "#09091a"
+    color: "#09091a" 
   },
   {
     iconName: "viber",
     brandName: "Viber",
-    color: "#59267c"
+    color: "#59267c" 
   },
   {
     iconName: "vimeo",
     brandName: "Vimeo",
-    color: "#1ab7ea"
+    color: "#1ab7ea" 
   },
   {
     iconName: "vk",
@@ -460,51 +460,49 @@ const brands = [
   {
     iconName: "vuejs",
     brandName: "Vue.js",
-    color: "#42b883"
+    color: "#42b883" 
   },
   {
     iconName: "whatsapp",
     brandName: "WhatsApp",
-    color: "#075e54"
+    color: "#075e54" 
   },
   {
     iconName: "wikipedia-w",
     brandName: "Wikipedia",
-    color: "#000000"
+    color: "#000000" 
   },
   {
     iconName: "windows",
     brandName: "Microsoft Windows",
-    color: "#0078d7"
+    color: "#0078d7" 
   },
   {
     iconName: "wix",
     brandName: "Wix.com",
-    color: "#333333"
+    color: "#333333" 
   },
   {
     iconName: "wordpress",
     brandName: "WordPress",
-    color: "#21759b"
+    color: "#21759b" 
   },
   {
     iconName: "xbox",
     brandName: "Xbox",
-    color: "#52b043"
+    color: "#52b043" 
   },
   {
     iconName: "yahoo",
     brandName: "Yahoo!",
-    color: "#410093"
+    color: "#410093" 
   },
   {
     iconName: "youtube",
     brandName: "YouTube",
-    color: "#ff0000"
+    color: "#ff0000" 
   }
 ];
-
-
 let correct = 0;
 let total = 0;
 const totalDraggableItems = 5;
@@ -513,7 +511,6 @@ const totalMatchingPairs = 5; // Should be <= totalDraggableItems
 const scoreSection = document.querySelector(".score");
 const correctSpan = scoreSection.querySelector(".correct");
 const totalSpan = scoreSection.querySelector(".total");
-// const playAgainBtn = scoreSection.querySelector("#play-again-btn");
 
 const draggableItems = document.querySelector(".draggable-items");
 const matchingPairs = document.querySelector(".matching-pairs");
@@ -524,34 +521,32 @@ initiateGame();
 
 function initiateGame() {
   const randomDraggableBrands = generateRandomItemsArray(totalDraggableItems, brands);
-  const randomDroppableBrands = totalMatchingPairs < totalDraggableItems ? generateRandomItemsArray(totalMatchingPairs, randomDraggableBrands) : randomDraggableBrands;
-  const alphabeticallySortedRandomDroppableBrands = [...randomDroppableBrands].sort((a, b) => a.brandName.toLowerCase().localeCompare(b.brandName.toLowerCase()));
-
+  const randomDroppableBrands = totalMatchingPairs<totalDraggableItems ? generateRandomItemsArray(totalMatchingPairs, randomDraggableBrands) : randomDraggableBrands;
+  const alphabeticallySortedRandomDroppableBrands = [...randomDroppableBrands].sort((a,b) => a.brandName.toLowerCase().localeCompare(b.brandName.toLowerCase()));
+  
   // Create "draggable-items" and append to DOM
-  for (let i = 0; i < randomDraggableBrands.length; i++) {
+  for(let i=0; i<randomDraggableBrands.length; i++) {
     //הצגת האיקונים על המסך
     draggableItems.insertAdjacentHTML("beforeend", `
-        <i class="fab fa-${randomDraggableBrands[i].iconName} draggable" draggable="true" style="color: ${randomDraggableBrands[i].color};" id="${randomDraggableBrands[i].iconName}"></i>
-      `);
+      <i class="fab fa-${randomDraggableBrands[i].iconName} draggable" draggable="true" style="color: ${randomDraggableBrands[i].color};" id="${randomDraggableBrands[i].iconName}"></i>
+    `);
   }
-
+  
   // Create "matching-pairs" and append to DOM
-  for (let i = 0; i < alphabeticallySortedRandomDroppableBrands.length; i++) {
+  for(let i=0; i<alphabeticallySortedRandomDroppableBrands.length; i++) {
     matchingPairs.insertAdjacentHTML("beforeend", `
-        <div class="matching-pair">
-          <span class="label">${alphabeticallySortedRandomDroppableBrands[i].brandName}</span>
-          <span class="droppable" data-brand="${alphabeticallySortedRandomDroppableBrands[i].iconName}"></span>
-        </div>
-      `);
+      <div class="matching-pair">
+        <span class="label">${alphabeticallySortedRandomDroppableBrands[i].brandName}</span>
+        <span class="droppable" data-brand="${alphabeticallySortedRandomDroppableBrands[i].iconName}"></span>
+      </div>
+    `);
   }
-
+  
   draggableElements = document.querySelectorAll(".draggable");
   droppableElements = document.querySelectorAll(".droppable");
-
+  
   draggableElements.forEach(elem => {
     elem.addEventListener("dragstart", dragStart);
-    // elem.addEventListener("drag", drag);
-    // elem.addEventListener("dragend", dragEnd);
   });
   //העזיבה של הגרירה
   droppableElements.forEach(elem => {
@@ -573,19 +568,19 @@ function dragStart(event) {
 //Events fired on the drop target
 
 function dragEnter(event) {
-  if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+  if(event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
     event.target.classList.add("droppable-hover");
   }
 }
 
 function dragOver(event) {
-  if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+  if(event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
     event.preventDefault();
   }
 }
 
 function dragLeave(event) {
-  if (event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
+  if(event.target.classList && event.target.classList.contains("droppable") && !event.target.classList.contains("dropped")) {
     event.target.classList.remove("droppable-hover");
   }
 }
@@ -595,37 +590,31 @@ function drop(event) {
   event.target.classList.remove("droppable-hover");
   const draggableElementBrand = event.dataTransfer.getData("text");
   const droppableElementBrand = event.target.getAttribute("data-brand");
-  const isCorrectMatching = draggableElementBrand === droppableElementBrand;
+  const isCorrectMatching = draggableElementBrand===droppableElementBrand;
   total++;
-  if (isCorrectMatching) {
+  if(isCorrectMatching) {
     const draggableElement = document.getElementById(draggableElementBrand);
     event.target.classList.add("dropped");
     draggableElement.classList.add("dragged");
     draggableElement.setAttribute("draggable", "false");
-    //קיבוע תמונה נכונה במסגרת 
     event.target.innerHTML = `<i class="fab fa-${draggableElementBrand}" style="color: ${draggableElement.style.color};"></i>`;
-    correct++;
+    correct++;  
     if (correct == 5 && (total >= 5 && total < 8)) {
       window.location.replace("win.html");
 
     }
   }
-
   else {
-    // mistakes[num] = {
-    //   icon: draggableItems,
-    //   mistake: droppableElementBrand,
-    //   good: draggableElementBrand
-    // };
-    let text1="fab-brands-fa";
-    let text2=draggableElementBrand;
-    let result=text1.concat(text2);
+    // let text1="fab fa-";
+    // let text2=draggableElementBrand;
+    // let result=text1.concat(text2);
+    var iconOfWord=`<i class="fab fa-${draggableElementBrand}></i>`;
     mistakes[num] = {
-      icon: result,
+      icon: iconOfWord,
       mistake: droppableElementBrand,
       good: draggableElementBrand
     };
-    localStorage.setItem("mistakes", JSON.stringify(mistakes[num].mistake+" "+mistakes[num].good));
+    // localStorage.setItem("mistakes", JSON.stringify(mistakes[num].mistake+" "+mistakes[num].good));
     num++;
     element = document.getElementById("heart");
     element.remove();
@@ -645,45 +634,15 @@ function drop(event) {
     totalSpan.textContent = total;
     scoreSection.style.opacity = 1;
   }, 200);
-  // if (correct === Math.min(totalMatchingPairs, totalDraggableItems)) { // Game Over!!
-  //   playAgainBtn.style.display = "block";
-  //   setTimeout(() => {
-  //     playAgainBtn.classList.add("play-again-btn-entrance");
-  //   }, 200);
-  // }
 }
-
-//Other Event Listeners
-// playAgainBtn.addEventListener("click", playAgainBtnClick);
-// function playAgainBtnClick() {
-//   playAgainBtn.classList.remove("play-again-btn-entrance");
-//   correct = 0;
-//   total = 0;
-//   draggableItems.style.opacity = 0;
-//   matchingPairs.style.opacity = 0;
-//   setTimeout(() => {
-//     scoreSection.style.opacity = 0;
-//   }, 100);
-setTimeout(() => {
-  // playAgainBtn.style.display = "none";
-  while (draggableItems.firstChild) draggableItems.removeChild(draggableItems.firstChild);
-  while (matchingPairs.firstChild) matchingPairs.removeChild(matchingPairs.firstChild);
-  initiateGame();
-  correctSpan.textContent = correct;
-  totalSpan.textContent = total;
-  draggableItems.style.opacity = 1;
-  matchingPairs.style.opacity = 1;
-  scoreSection.style.opacity = 1;
-}, 500);
-// }
 
 // Auxiliary functions
 function generateRandomItemsArray(n, originalArray) {
   let res = [];
   let clonedArray = [...originalArray];
-  if (n > clonedArray.length) n = clonedArray.length;
-  for (let i = 1; i <= n; i++) {
-    const randomIndex = Math.floor(Math.random() * clonedArray.length);
+  if(n>clonedArray.length) n=clonedArray.length;
+  for(let i=1; i<=n; i++) {
+    const randomIndex = Math.floor(Math.random()*clonedArray.length);
     res.push(clonedArray[randomIndex]);
     clonedArray.splice(randomIndex, 1);
   }
