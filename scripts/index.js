36fa62621ef0a17293ac3030d534fb21e0de6291
Avigail Siman Tov <avigailsi123@gmail.com>
2022-08-31 02:11:18 +0300

    סידור דף

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,18 @@
 const axios = require('axios').default;
+
 const express = require('express')
 const app = express()
+
 const PORT = process.env.PORT || 3001
+
+app.get("/");
+
 
 app.listen(PORT)
 console.log("server is listening in port 3001")
 
 app.use(express.static('static'))
-app.use('/', express.static('index.html'))
-// app.use(express.json())
+app.use('/', express.static('enter.html'))
+app.use(express.json())
 
 
